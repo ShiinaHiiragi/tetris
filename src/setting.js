50bc6897de9f6e8aac87b2a89780a62055a1fb95
Shiina <shiinahiiragi@outlook.com>
2021-02-19 16:00:15 +0800

    add setting

---
+++ src/setting.js
@@ -0,0 +1,23 @@
+var Setting =
+{
+  color:
+    [
+      "rgb(225, 225, 225)", "rgb(252, 92, 101)",
+      "rgb(253, 150, 68)", "rgb(254, 211, 48)",
+      "rgb(38, 222, 129)", "rgb(69, 170, 242)",
+      "rgb(75, 123, 236)", "rgb(165, 94, 234)"
+    ],
+  keyDownCode:
+  {
+    left: 37, right: 39, hold: 16,
+    leftHalf: 88, rightHalf: 67,
+    leftQuarter: 90, rightQuarter: 86,
+    hardDrop: 38, softDrop: 40
+  },
+  debugMode: false,
+  bgm: 3,
+  countDown: 500,
+  infoTime: 1000,
+  fallTime: 60000,
+  lockTime: 500
+}
